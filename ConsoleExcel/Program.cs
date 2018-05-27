522c500fb11073d15f034e506832d4cd51f1b246
Arman <arman4u@gmail.com>
2018-05-27 21:37:30 +0200

    Add project files.

---
+++ ConsoleExcel/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel; 
+using _Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace ConsoleExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "file.xlsx";
+            _Application excel = new _Excel.Application();
+            Workbook wb = excel.Workbooks.Open(path);
+            Worksheet ws = excel.Worksheets[1];
+
+            Console.WriteLine(ws.Cells[1,1].Value2);
+
+            ws.Cells[4, 7] = "Arman!";
+            wb.SaveAs("new2.xlsx");
+            wb.Close();
+
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+    }
+}
